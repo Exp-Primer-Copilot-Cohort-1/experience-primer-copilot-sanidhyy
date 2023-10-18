c651f5ba92a2fea8fc1e009d87ac0e5191d5398f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-18 15:09:12 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,52 +1,31 @@
-// create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-// import data
-const { comments } = require('./data/comments');
-// import functions
-const { findById, findByMovieId, createNewComment } = require('./utils/comments');
+// Create web server
+// Run: node comments.js
+// Load: http://localhost:3000/
+// Load: http://localhost:3000/comments
+// Load: http://localhost:3000/comments/1
 
-// middleware
-app.use(express.json());
+// Load the express module
+var express = require('express');
 
-// GET /comments - returns all comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
+// Create an express app
+var app = express();
+
+// Create a route for the home page
+app.get('/', function(req, res) {
+    res.send('Welcome to the home page!');
 });
 
-// GET /comments/:id - returns a single comment by id
-app.get('/comments/:id', (req, res) => {
-  const comment = findById(comments, req.params.id);
-  if (!comment) {
-    res.status(404).json({ msg: `Comment with id ${req.params.id} not found.` });
-    return;
-  }
-  res.json(comment);
+// Create a route for the /comments page
+app.get('/comments', function(req, res) {
+    res.send('Welcome to the comments page!');
 });
 
-// GET /comments/movie/:movieId - returns all comments for a movie by movie id
-app.get('/comments/movie/:movieId', (req, res) => {
-  const movieComments = findByMovieId(comments, req.params.movieId);
-  if (!movieComments) {
-    res.status(404).json({ msg: `No comments for movie with id ${req.params.movieId} found.` });
-    return;
-  }
-  res.json(movieComments);
+// Create a route for the /comments/1 page
+app.get('/comments/1', function(req, res) {
+    res.send('Welcome to the first comment!');
 });
 
-// POST /comments - creates a new comment
-app.post('/comments', (req, res) => {
-  const { commenter, movieId, comment } = req.body;
-  if (!commenter || !movieId || !comment) {
-    res.status(400).json({ msg: 'Please include a commenter, movieId, and comment.' });
-    return;
-  }
-  const newComment = createNewComment(comments, commenter, movieId, comment);
-  res.json(newComment);
-});
-
-// start server
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+// Start the server
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
 });
